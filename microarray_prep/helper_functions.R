d410615575cf4800224b9b79c9e3f7a1d16c4d68
essharom <kamp.sebi@gmail.com>
2020-10-22 17:43:29 +0200

    slight mods

--- microarray_prep/helper_functions.R
+++ microarray_prep/helper_functions.R
@@ -2,7 +2,6 @@
 
 #libraries
 library("tidyverse")
-
 library("affy")
 library("biomaRt")
 
@@ -18,6 +17,8 @@ library("biomaRt")
 ensembl = useEnsembl(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
 attributes = listAttributes(ensembl)
 
+
+# calculate abundances for a specific condition
 run_condition <- function(condition_name, df){
   print(condition_name)
   condition_df <- df %>% 
@@ -25,14 +26,17 @@ run_condition <- function(condition_name, df){
   
   platforms <- unique(condition_df$platform)
   
+  # TODO median aggregation seems like a good option, but for aggregating probabilities, possibly some better function exists
   result <- map_dfr(platforms, run_platform, condition_df) %>% 
     group_by(uniprotswissprot) %>% 
-    summarise(expression = median(expression)) %>%  # TODO aggregating probabilities, possibly some better function exists
+    summarise(expression = median(expression)) %>%  
     mutate(condition = condition_name)
   
   return(result)
 }
 
+
+# calculate abundances for a platform
 run_platform <- function(platform_name, df){
   print(platform_name)
   
@@ -41,12 +45,15 @@ run_platform <- function(platform_name, df){
   
   platform_fun <- get_platform_function(platform_name)
   
+  # scale all values on (0 to 1) scale by scales::rescale function
   result <- platform_fun(platform_samples_df$file_path) %>%
     mutate(expression = scales::rescale(expression, to = c(0, 1)))
   
   return(result)
 }
 
+
+# map the correct platform specific function to the CEL file analysis
 get_platform_function <- function(platform_name){
   if (platform_name == 'GPL570'){
     return (GPL570_read)
@@ -56,7 +63,25 @@ get_platform_function <- function(platform_name){
     return (GPL571_read)
   }
   
+  # TODO
+  # if (platform_name == 'GPL17692'){
+  #   return (GPL17692_read)
+  # }
+  
 }
+
+
+
+#
+# FUNCTION blueprint
+# <platform_name>_read <- function(file_list){ 
+#
+#
+# output: dataframe of <uniprotswissprot> and <expression> values
+# }
+#
+# add function name to get_platform_function
+#
 
 
 # TODO function to summarize by sample (median) and by protein (sum) instead of doing it at every platform
@@ -69,72 +94,120 @@ get_platform_function <- function(platform_name){
 # }
 
 
+
+affy_workflow <- function(file_list, affy_cdfname, biomart_attribute_name){
+  raw_data = ReadAffy(verbose=TRUE, filenames = file_list, cdfname = affy_cdfname) # read the row data to raw.data
+  rma_df = rma(raw_data) # mas5(); normalise the raw data 
+  expression_df = exprs(rma_df)	%>% 
+    as.data.frame() # get the normalised data in a readable matrix
+  
+  probes = expression_df %>% 
+    mutate(probe_id = row.names(expression_df))
+  
+  annot <- getBM(attributes=c(biomart_attribute_name,'uniprotswissprot'), mart = ensembl) %>% 
+    dplyr::rename(probe_id = biomart_attribute_name)
+  
+  affy <- annot %>% 
+    filter(uniprotswissprot!="",
+           probe_id!="") %>% 
+    left_join(probes) %>%
+    dplyr::select(!probe_id) %>% 
+    group_by(uniprotswissprot) %>% 
+    summarise_all(sum) %>%
+    ungroup() %>% 
+    pivot_longer(cols = !uniprotswissprot, names_to = "sample_id", values_to = "expression") %>% 
+    group_by(uniprotswissprot) %>% 
+    summarise(expression = median(expression))
+  
+}
+
+
 #GPL570 
 #library("hgu133plus2.db") #GPL570 platform
 
 #file_list = sample_df$file_path[1:2]
 
 GPL570_read <- function(file_list){
-  raw_data = ReadAffy(verbose=TRUE, filenames=file_list, cdfname="HG-U133_Plus_2") # read the row data to raw.data
-  rma_df = rma(raw_data) # mas5(); normalise the raw data 
-  expression_df = exprs(rma_df)	%>% 
-    as.data.frame() # get the normalised data in a readable matrix
-  
-  probes = expression_df %>% 
-    mutate(probe_id = row.names(expression_df))
-
-  annot <- getBM(attributes=c('affy_hg_u133_plus_2','uniprotswissprot'), mart = ensembl) %>% 
-    dplyr::rename(probe_id = affy_hg_u133_plus_2)
-  
-  affy <- annot %>% 
-    filter(uniprotswissprot!="",
-           probe_id!="") %>% 
-    left_join(probes) %>%
-    dplyr::select(!probe_id) %>% 
-    group_by(uniprotswissprot) %>% 
-    summarise_all(sum) %>%
-    ungroup() %>% 
-    pivot_longer(cols = !uniprotswissprot, names_to = "sample_id", values_to = "expression") %>% 
-    group_by(uniprotswissprot) %>% 
-    summarise(expression = median(expression))
+  affy_workflow(file_list = file_list,
+                affy_cdfname = "HG-U133_Plus_2",
+                biomart_attribute_name = 'affy_hg_u133_plus_2')
 }
 
+
+
+
+
+# GPL570_read <- function(file_list){
+#   raw_data = ReadAffy(verbose=TRUE, filenames=file_list, cdfname="HG-U133_Plus_2") # read the row data to raw.data
+#   rma_df = rma(raw_data) # mas5(); normalise the raw data 
+#   expression_df = exprs(rma_df)	%>% 
+#     as.data.frame() # get the normalised data in a readable matrix
+#   
+#   probes = expression_df %>% 
+#     mutate(probe_id = row.names(expression_df))
+# 
+#   annot <- getBM(attributes=c('affy_hg_u133_plus_2','uniprotswissprot'), mart = ensembl) %>% 
+#     dplyr::rename(probe_id = affy_hg_u133_plus_2)
+#   
+#   affy <- annot %>% 
+#     filter(uniprotswissprot!="",
+#            probe_id!="") %>% 
+#     left_join(probes) %>%
+#     dplyr::select(!probe_id) %>% 
+#     group_by(uniprotswissprot) %>% 
+#     summarise_all(sum) %>%
+#     ungroup() %>% 
+#     pivot_longer(cols = !uniprotswissprot, names_to = "sample_id", values_to = "expression") %>% 
+#     group_by(uniprotswissprot) %>% 
+#     summarise(expression = median(expression))
+# }
+# 
 
 #GPL571 
-library("hgu133a2.db") #GPL571 platform
+#library("hgu133a2.db") #GPL571 platform
 
 GPL571_read <- function(file_list){
-  raw_data = ReadAffy(verbose=TRUE, filenames=file_list, cdfname="HG-U133A_2", compress = TRUE) # read the row data to raw.data
-  rma_df = rma(raw_data) # mas5(); normalise the raw data 
-  expression_df = exprs(rma_df)	%>% 
-    as.data.frame() # get the normalised data in a readable matrix
-  
-  probes = expression_df %>% 
-    mutate(probe_id = row.names(expression_df))
-  
-  annot <- getBM(attributes=c('affy_hg_u133a_2','uniprotswissprot'), mart = ensembl) %>% 
-    dplyr::rename(probe_id = affy_hg_u133a_2)
-  
-  affy <- annot %>% 
-    filter(uniprotswissprot!="",
-           probe_id!="") %>% 
-    left_join(probes) %>%
-    dplyr::select(!probe_id) %>% 
-    group_by(uniprotswissprot) %>% 
-    summarise_all(sum) %>%
-    ungroup() %>% 
-    pivot_longer(cols = !uniprotswissprot, names_to = "sample_id", values_to = "expression") %>% 
-    group_by(uniprotswissprot) %>% 
-    summarise(expression = median(expression))
-  
+  affy_workflow(file_list = file_list,
+                affy_cdfname = "HG-U133A_2",
+                biomart_attribute_name = 'affy_hg_u133a_2')
 }
 
 
-# <platform_name>_read <- function(file_list){ 
-#
-#
-# output: dataframe of <uniprotswissprot> and <expression> values
+# GPL571_read <- function(file_list){
+#   raw_data = ReadAffy(verbose=TRUE, filenames=file_list, cdfname="HG-U133A_2", compress = TRUE) # read the row data to raw.data
+#   rma_df = rma(raw_data) # mas5(); normalise the raw data 
+#   expression_df = exprs(rma_df)	%>% 
+#     as.data.frame() # get the normalised data in a readable matrix
+#   
+#   probes = expression_df %>% 
+#     mutate(probe_id = row.names(expression_df))
+#   
+#   annot <- getBM(attributes=c('affy_hg_u133a_2','uniprotswissprot'), mart = ensembl) %>% 
+#     dplyr::rename(probe_id = affy_hg_u133a_2)
+#   
+#   affy <- annot %>% 
+#     filter(uniprotswissprot!="",
+#            probe_id!="") %>% 
+#     left_join(probes) %>%
+#     dplyr::select(!probe_id) %>% 
+#     group_by(uniprotswissprot) %>% 
+#     summarise_all(sum) %>%
+#     ungroup() %>% 
+#     pivot_longer(cols = !uniprotswissprot, names_to = "sample_id", values_to = "expression") %>% 
+#     group_by(uniprotswissprot) %>% 
+#     summarise(expression = median(expression))
+#   
 # }
-#
-# add function name to get_platform_function
-#
+
+
+# GPL17692	
+#	[HuGene-2_1-st] Affymetrix Human Gene 2.1 ST Array [transcript (gene) version]
+
+
+# TODO
+# GPL17692_read <- function(file_list){
+#   affy_workflow(file_list = file_list,
+#                 affy_cdfname = "HuGene-2_1-st",
+#                 biomart_attribute_name = 'affy_hugene_2_0_st_v1')
+# }
+
--- microarray_prep/main.R
+++ microarray_prep/main.R
@@ -6,23 +6,37 @@ sample_root = "C://Users/sebes/Dropbox/linkgroup/signaling2020_sample/"
 
 sample_cels = list.files(path=sample_root, pattern = "(CEL|cel)(.gz)?$", recursive = TRUE)
 
-
+#create design matrix for all samples used in the whole project
 sample_df <- sample_cels %>% 
   str_split_fixed(.,pattern = "/", n = 5) %>% 
   as.data.frame() %>% 
   `colnames<-`(c("tissue_type", "condition", "platform", "project", "sample_id")) %>% 
   mutate(file_path = paste0(sample_root, sample_cels))
 
+#GPL17692_read function must be 
 
 tissue_sample_df <- sample_df %>% 
-  filter(tissue_type == tissue_type)
+  filter(tissue_type == tissue_type) %>% 
+  filter(platform != "GPL17692")
 
 conditions <- unique(tissue_sample_df$condition)
 
+# run abundance calculation for a tissue type, call necessary functions
 result_df <- map_dfr(conditions, run_condition, tissue_sample_df)
 
+# make table human readable
 result_df_wide <- result_df %>% 
   pivot_wider(names_from = condition, values_from = expression)
 
+# TODO add HUGO GENE SYMBOLS to the `wide` dataframe possibly with biomart
+annot_hgnc <- getBM(attributes=c('hgnc_symbol','uniprotswissprot'), mart = ensembl)
+
+result_df_wide <- result_df_wide
+
+# SUPPL_RESULT_Table
+# write out result matrix with abundances
 write_tsv(result_df_wide, paste0(tissue_type, "_abundances.tsv"))
+
+# SUPPL_Table
+# write out design matrix with all used samples for given tissue type
 write_tsv(tissue_sample_df, paste0(tissue_type, "_files.tsv"))
